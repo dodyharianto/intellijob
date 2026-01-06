0ffb2e4d66c03bc09ac3eb69f67e919fa0a0b97a
dodyharianto <hariantodody14@gmail.com>
2026-01-06 17:05:01 +0800

    add function to init vector store and use langchain document for chunking

--- src/rag.py
+++ src/rag.py
@@ -1,33 +1,34 @@
 from langchain_community.document_loaders import PyPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain_openai import OpenAIEmbeddings, ChatOpenAI
+from langchain_openai import OpenAIEmbeddings
 from langchain_chroma import Chroma
-from chromadb import PersistentClient
+from langchain_core.documents import Document
+from typing import List
 import os
 from dotenv import load_dotenv
-from src.memory import get_current_timestamp
+from memory import get_current_timestamp
 
 load_dotenv()
-embedding_dir = os.environ.get("EMBEDDING_DB_DIR")
+EMBEDDING_DIR = os.environ.get("EMBEDDING_DB_DIR")
+EMBEDDING_MODEL = OpenAIEmbeddings(model='text-embedding-3-small')
 
-def clear_collection(collection_name):
-    client = PersistentClient(path=embedding_dir)
-    collection = client.get_collection(name=collection_name)
-    print(f'Found collection {collection_name}')
-
-    document_ids = collection.get()['ids']
-    if len(document_ids) > 0:
-        collection.delete(ids=document_ids)
-    print(f'Collection {collection_name} successfully cleared.')
+def get_vector_store(collection_name: str):
+    return Chroma(
+        collection_name=collection_name,
+        embedding_function=EMBEDDING_MODEL,
+        persist_directory=EMBEDDING_DIR
+    )
     
-def embed_documents(collection_name, root_folder='docs'):
-    clear_collection(collection_name)
-    docs = os.listdir(root_folder)
-    print(docs)
+def embed_documents(document_dir: str = './docs', collection_name: str = 'user_profile') -> List[Document]:
+    vector_store = get_vector_store(collection_name)
+    existing_chunk_ids = vector_store.get()['ids']
+    if existing_chunk_ids:
+        vector_store.delete(ids=existing_chunk_ids)
 
+    docs = os.listdir(document_dir)
     all_chunks = []
     for doc in docs:
-        file_path = os.path.join(root_folder, doc)
+        file_path = os.path.join(document_dir, doc)
         pdf_loader = PyPDFLoader(file_path)
         document = pdf_loader.load()
 
@@ -40,29 +41,16 @@ def embed_documents(collection_name, root_folder='docs'):
         now = get_current_timestamp()
         for idx, chunk in enumerate(chunks):
             chunk.metadata.update({
-                'chunk_id': idx,
-                'creation_date': now.strftime('%Y-%m-%d %H:%M:%S'),
+                'creationdate': now.strftime('%Y-%m-%d %H:%M:%S'),
+                'moddate': now.strftime('%Y-%m-%d %H:%M:%S'),
             })
 
+        vector_store.add_documents(chunks)
         all_chunks.extend(chunks)
 
-    embeddings = OpenAIEmbeddings(model='text-embedding-3-small')
-    vector_store = Chroma.from_documents(
-        documents=all_chunks,
-        embedding=embeddings,
-        persist_directory=embedding_dir,
-        collection_name=collection_name
-    )
+    return all_chunks
 
-    print(f'Added {len(all_chunks)} chunks to vector database.')
-
-def retrieve_chunks(collection_name, query, k=3):
-    embeddings = OpenAIEmbeddings(model="text-embedding-3-small")
-    vector_store = Chroma(
-        embedding_function=embeddings,
-        persist_directory=embedding_dir,
-        collection_name=collection_name
-    )
-
+def retrieve_chunks(query: str, k: int = 3, collection_name: str = 'user_profile') -> List[Document]:
+    vector_store = get_vector_store(collection_name)
     results = vector_store.similarity_search(query, k=k)
     return results
